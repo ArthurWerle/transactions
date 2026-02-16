58fff81a32fffb698afc2907243a361358c1bd7c
arthurwerle <arthur.werle@gmail.com>
2026-02-16 13:36:35 -0300

    remove omitempty

--- internal/model/category.go
+++ internal/model/category.go
@@ -10,7 +10,7 @@ type Category struct {
 	ID          uint           `gorm:"primaryKey" json:"id"`
 	CreatedAt   time.Time      `gorm:"column:created_at" json:"created_at"`
 	UpdatedAt   time.Time      `gorm:"column:updated_at" json:"updated_at"`
-	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted_at"`
 	Name        string         `gorm:"column:name;not null;size:255" json:"name"`
 	Description string         `gorm:"column:description;type:text" json:"description"`
 	Color       string         `gorm:"column:color;size:50" json:"color"`
